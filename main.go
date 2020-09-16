12985236013e1453b52d7deebace4fb1da7fed80
Sanket Gupta <sanketgupta07@gmail.com>
2020-09-17 00:19:13 +0530

    merged

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-<<<<<<< HEAD
 	closuers "github.com/sanketgupta07/go-practice/closures"
-=======
->>>>>>> cba3d14966d59ac5e65ad458deb1bfbcf2b3173f
 	multiplevalue "github.com/sanketgupta07/go-practice/return-multiple-value"
 	variadic "github.com/sanketgupta07/go-practice/variadic_function"
 
@@ -16,8 +13,5 @@ func main() {
 	log.Println("Main function")
 	log.Println(multiplevalue.MultipleValue())
 	log.Println("Sum: ", variadic.VariadicFunc(2, 5))
-<<<<<<< HEAD
 	log.Println("Calling CallClosures", closuers.CallClosures(10))
-=======
->>>>>>> cba3d14966d59ac5e65ad458deb1bfbcf2b3173f
 }
